1f8ee78e6500c54103cd2fe426709ef6db0bc8d5
spbryan <seanbryan@comcast.com>
2019-05-26 10:01:33 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+/***********************************************
+ * Project: My Guitar Hub
+ * Developer: Sean Bryan
+ * Date: 2019-05-26
+ ***********************************************/
+
+$(document).ready(function () {
+
+    /** On-Click for topic buttons */
+    // $(document).on("click", ".topic-button", retrieveGifs);
+});
