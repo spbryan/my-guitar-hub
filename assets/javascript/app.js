8ac56d050ff05c59e707ed7a404e5b41d3806366
spbryan <seanbryan@comcast.com>
2019-05-26 14:09:24 -0400

    populate table from user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,30 @@
  * Developer: Sean Bryan
  * Date: 2019-05-26
  ***********************************************/
-
- //JQuery References
-var title = $("#song-title-input");
-var artist = $("#artist-input");
-var genre = $("#genre-input");
-var proficiency = $("#proficiency-input");
-
 $(document).ready(function () {
+    //JQuery References
+    var title = $("#song-title-input");
+    var artist = $("#artist-input");
+    var genre = $("#genre-input");
+    var proficiency = $("#proficiency-input");
 
+    //Firebase Configurations
+    var firebaseConfig = {
+        apiKey: "AIzaSyDQuKbESj040xKw40aHacSDni_cbbp0QBI",
+        authDomain: "guitar-songs-ca432.firebaseapp.com",
+        databaseURL: "https://guitar-songs-ca432.firebaseio.com",
+        projectId: "guitar-songs-ca432",
+        storageBucket: "guitar-songs-ca432.appspot.com",
+        messagingSenderId: "953844895516",
+        appId: "1:953844895516:web:c2acec717f58abe1"
+    };
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
+    /**
+     * Function that captures song input details from modal to be 
+     * saved to the database
+     */
     function addASong() {
         event.preventDefault();
 
@@ -26,15 +41,65 @@ $(document).ready(function () {
         genre.val("");
         proficiency.val("");
 
-        console.log(song);
-
-        // if (userid !== "") {
-        //     comicbookRef = firebase.database().ref(userid + "/comicbooks");
-        //     comicbookRef.push(newComic);
-        // } else {
-        //     displayInventory(newComic, null);
-        // };
+        if (title && artist) {
+            addSongToDatabase(song);
+        }
     }
+
+    function displayInventory(snapshot) {
+        $("#table-body").empty();
+        snapshot.forEach(function(childSnapshot) {
+            childSnapshot.forEach(function(song) {
+                var inventoryId = song.key;
+                console.log(inventoryId);
+                var newRow = $("<tr>");
+                newRow.addClass("inventory-item table-entry");
+                newRow.append(displayTableEntry("table-title", song.val().title));
+                newRow.append(displayTableEntry("table-artist", song.val().artist));
+                newRow.append(displayTableEntry("table-genre", song.val().genre));
+                newRow.append(displayTableEntry("table-proficiency", song.val().proficiency));
+                if (null != song.val().title) {
+                    newRow.attr("id", inventoryId);
+                    var removeBtn = $("<button>", { class: "btn waves-effect remove", id: inventoryId, text: "Remove" });
+                    newRow.append(removeBtn);
+                }
+                else {
+                    var emptyTable = $("<td>");
+                    newRow.append(emptyTable)
+                }
+
+                // Append the new row to the table
+                $("#song-list-table > tbody").prepend(newRow);
+            });
+        });
+    }
+
+    /**
+    * Populate a new table cell, assign an id, and return the value
+    * @param id 
+    * @param value 
+    */
+    function displayTableEntry(id, value) {
+        var newEntry = $("<td>");
+        newEntry.attr("id", id);
+        newEntry.text(value);
+        return newEntry;
+    }
+
+    function addSongToDatabase(song) {
+        var row = database.ref("/song/");
+
+        row.push(song);
+    }
+
+    /**
+     * Listens for changes to the values on the data base
+    */
+    database.ref().on("value", function (snapshot) {
+        displayInventory(snapshot);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
 
     /** On-Click for topic buttons */
     $(document).on("click", ".add-song-button", addASong);
