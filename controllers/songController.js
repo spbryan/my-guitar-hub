9d136d90f1f4ce8dcdb11c0fc7ad098f471e93e9
spbryan <seanbryan@comcast.com>
2019-09-28 07:17:59 -0400

    add api routes for mondodb

---
+++ controllers/songController.js
@@ -0,0 +1,38 @@
+/********************************
+ * Controller directing data manipulation for Song Model
+ * 
+ * @author Sean Bryan
+ * 
+ * 2019-09-28
+ ********************************/
+const db = require("../models");
+
+// Defining methods for the Controller
+module.exports = {
+  findAll: function(req, res) {
+    db.Song.find(req.query)
+      .then(dbSong => res.json(dbSong))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Song.findById(req.params.id)
+      .then(dbSong => res.json(dbSong))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Song.create(req.body)
+      .then(dbSong => res.json(dbSong))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Song.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbSong => res.json(dbSong))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Song.findById(req.params.id)
+      .then(dbSong => dbSong.remove())
+      .then(dbSong => res.json(dbSong))
+      .catch(err => res.status(422).json(err));
+  }
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +8,23 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/myguitarhub",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Send every other request to the React app
 // Define any API routes before this runs
